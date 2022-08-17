6226b42a7229cfa4a5c9f97eee5e1ffdd1d6ad81
Chris B <cbosoft@protonmail.com>
2022-08-17 14:59:53 +0100

    redact image directly (without adding annotation first)

--- redactor/redact.py
+++ redactor/redact.py
@@ -19,21 +19,36 @@ class Redactor:
         config = get_config()
         config.merge_from_file(f'{res_dir}/config.yaml')
         config.model.weights = f'{res_dir}/model_state_final.pth'
+        self.device = config.training.device
 
         self.model = build_model(config)
         self.model.eval()
 
+        self.ds = AutoAnnotatedImagesDataset.from_config(config)
+
     def redact_image(self, image):
+        if isinstance(image, str):
+            colour_image = cv2.imread(image, cv2.IMREAD_COLOR)
+            image = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+            image = image[np.newaxis, ...]
+            image = image.astype(float)/255.
+            image = torch.tensor(image).float()
+        else:
+            colour_image = (image.cpu().numpy()*255).astype('uint8').squeeze()
+            if len(colour_image.shape < 3):
+                colour_image = cv2.cvtColour(colour_image, cv2.COLOR_GRAY2BGR)
+
         with torch.no_grad():
             out = self.model([image])[0]
-        npy_img = (image.cpu().numpy()*255).astype('uint8').squeeze()
+
         for bbox in out['boxes']:
             x0, y0, x1, y1 = [int(v) for v in bbox]
-            cv2.rectangle(npy_img, (x0-5, y0-10, x1-x0+5, y1-y0+10), 0, -1)
-        return npy_img
+            cv2.rectangle(colour_image, (x0, y0, x1-x0, y1-y0), 0, -1)
+
+        return colour_image
 
     def annot_and_redact(self, fn: str, sz=None):
-        rand_image, rand_target = AutoAnnotatedImagesDataset.get_annotated(fn, sz)
+        rand_image, rand_target = self.ds.get_annotated(fn, sz)
         redacted = self.redact_image(rand_image)
 
         fig, (oax, tax, rax) = plt.subplots(ncols=3, figsize=(15, 5))
